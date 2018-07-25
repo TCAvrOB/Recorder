8f5acb071bc78addec18222989fa2668424d2aaf
kerrot <kerrot.gs@gmail.com>
2018-07-25 20:38:31 +0900

    init

---
+++ Assets/Fire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+    [SerializeField] Vector3 power;
+
+    bool fire;
+
+    // Update is called once per frame
+    void Update() {
+        if (!fire && !Record.Recording && Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().AddForce(power);
+            Record.StartRecord();
+            fire = true;
+        }
+
+        if (fire && Record.Recording == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Play Record");
+
+        }
+    }
+}
---
+++ Assets/Record.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.Animations;
+
+public class Record : MonoBehaviour {
+    public AnimationClip clip;
+
+    public static bool Recording { get; private set; }
+    static float RecordTime = 5f;
+    static float StartTime = 0;
+    GameObjectRecorder recorder;
+
+	// Use this for initialization
+	void Start () {
+        recorder = new GameObjectRecorder(gameObject);
+        recorder.BindComponent<Transform>(gameObject, true);
+        clip.ClearCurves();
+	}
+	
+    public static void StartRecord()
+    {
+        Debug.Log("Start Record");
+        Recording = true;
+        StartTime = Time.time;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Recording)
+        {
+            recorder.TakeSnapshot(Time.deltaTime);
+
+            if (Time.time - StartTime > RecordTime)
+            {
+                Debug.Log("End Record");
+                Recording = false;
+                recorder.SaveToClip(clip);
+                GetComponent<Animator>().speed = 0.5f;
+            }
+        }
+    }
+}
